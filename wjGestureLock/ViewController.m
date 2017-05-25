0020680e4579ad85b9f318dd78241cc8f398fcc1
gouzi <gouzi@wangjun.local>
2017-05-25 09:26:08 +0800

    添加水印

--- wjGestureLock/ViewController.m
+++ wjGestureLock/ViewController.m
@@ -11,15 +11,34 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+    [self wjAddNameMarkInPicture];
 }
+
+- (void)wjAddNameMarkInPicture {
+    UIImage *image = [UIImage imageNamed:@"gagi.jpg"];
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, 0);
+    [image drawAtPoint:CGPointZero];
+    NSString *markStr = @"@请输入账号名";
+    NSDictionary *dict = @{
+                           NSForegroundColorAttributeName : [UIColor redColor],
+                           NSFontAttributeName : [UIFont systemFontOfSize:25.f]
+                           };
+    [markStr drawAtPoint:CGPointMake(image.size.width - markStr.length * 25.0f - 10, image.size.height - 35) withAttributes:dict];
+    UIImage *newImg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.imageView.image = newImg;
+}
+
+
+
+
 
 - (IBAction)wjReDrawGestureCodeAction:(UIButton *)sender {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
